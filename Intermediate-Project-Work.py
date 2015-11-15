4522f4bde0ff525a37545030a67689c551369d7c
codefetish <vishaalmohan@gmail.com>
2015-11-15 02:23:51 -0600

    Dummy commit

--- Intermediate-Project-Work.py
+++ Intermediate-Project-Work.py
@@ -1,24 +1,8 @@
 
-# coding: utf-8
-
-# In[1]:
-
 import numpy as np
-
-
-# In[2]:
-
-dataset = []
-for filename in ["./dataset_raw/semeval2016-task6-trialdata.txt", "./dataset_raw/semeval2016-task6-trainingdata.txt"]:
-    f = open(filename, 'r')
-    dataset  = dataset + f.readlines()
-
-print "Number of items = ", len(dataset)
-
-
-# In[3]:
-
-#glove_word_vec_file = "/Users/bhargav/Downloads/glove.twitter.27B/glove.twitter.27B.25d.txt"
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn import cross_validation
 
 def readGloveData(glove_word_vec_file):
     f = open(glove_word_vec_file, 'r')
@@ -31,21 +15,12 @@ def readGloveData(glove_word_vec_file):
         word_vec_dict[tag] = np.array(vec, dtype=float)
             
     return word_vec_dict
-            
-# word_vec_dict = readGloveData(glove_word_vec_file)
-
-
-# In[4]:
 
 def getWordVector(word, word_vec_dict):
     if word in word_vec_dict:
         return word_vec_dict[word]
     return np.zeros_like(word_vec_dict['hi'])
     
-
-
-# In[23]:
-
 def readTweets(fileName):
     file = open(fileName, 'r')
     raw_tweets = []
@@ -59,25 +34,18 @@ def readTweets(fileName):
         tweet2.append(tweet[0])
         tweet2.append(tweet[1])
         tweet2.extend(tweet[2].strip().replace(',',' ').replace('.',' ').replace(':', ' ').split(' '))
-        tweet2.append(tweet[3])
-        # print tweet2
-        # if tweet[1] != 'atheism':
-        #     continue
-        
+        tweet2.append(tweet[3])    
         tweets.append(tweet2[1:len(tweet2) - 1])
         tweetClass.append(tweet[len(tweet) - 1])
     tweets.append(tweetClass)
     return tweets
 
-def getTweetVectors(tweets):
+def getTweetVectors(tweets, word_vec_dict):
     tweetVectors = []
     for tweet in tweets:
-        #print getSumVectors(tweet, word_vec_dict)
-        print tweet
+        # print tweet
         tweetVectors.append(getSumVectors(tweet, word_vec_dict))
     return tweetVectors
-
-
 
 def getSumVectors(tweetData, word_vec_dict):
     numNonZero = 0
@@ -92,10 +60,7 @@ def getSumVectors(tweetData, word_vec_dict):
             numNonZero += 1
 
     for word in tweet:
-        print word
         vec = getWordVector(word, word_vec_dict)
-        #print word, vec
-        
         vector = vector + vec
         if vec.sum() != 0:
             numNonZero += 1
@@ -105,55 +70,35 @@ def getSumVectors(tweetData, word_vec_dict):
 
     return vector
 
+def splitTweets(tweets):
+    pass
+    
 
-# In[25]:
+def main():
+    word_vec_dict = readGloveData('./glove.twitter.27B/glove.twitter.27B.25d.txt')
+    tweets = readTweets('./dataset_raw/semeval2016-task6-trainingdata.txt')
 
-word_vec_dict = readGloveData('./glove.twitter.27B/glove.twitter.27B.25d.txt')
-tweets = readTweets('./dataset_raw/semeval2016-task6-trainingdata.txt')
+    tweetVectors = getTweetVectors(tweets[0:len(tweets) - 1], word_vec_dict)
+    print tweets[0]
+    print getSumVectors(tweets[0], word_vec_dict)
+    tweetClasses = set(tweets[-1])
 
-tweetVectors = getTweetVectors(tweets[0:len(tweets) - 1])
-print tweets[0]
-print getSumVectors(tweets[0], word_vec_dict)
+    mapping = {'favor': 1, 'none': 0, 'against': 1}
 
+    tweetClasses = np.asarray([mapping[x] for x in tweets[-1]])
+    tweetData = np.asarray(tweetVectors)
+    print tweetClasses.shape
+    print tweetData.shape
+    X = tweetData
+    Y = tweetClasses
+    clf = OneVsRestClassifier(LinearSVC(random_state=0))
+    # X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.3, random_state=0)
+    X_train = X[0:int(0.7 * len(X))]
+    y_train = Y[0:int(0.7 * len(Y))]
+    X_test = X[int(0.7 * len(X)) : len(X)]
+    y_test = Y[int(0.7 * len(Y)) : len(Y)]
+    clf.fit(X_train, y_train)
+    print clf.score(X_test, y_test)
 
-# In[26]:
-
-tweetClasses = set(tweets[-1])
-
-mapping = {'favor': 1, 'none': 0, 'against': 1}
-
-tweetClasses = np.asarray([mapping[x] for x in tweets[-1]])
-tweetData = np.asarray(tweetVectors)
-
-print tweetClasses.shape
-print tweetData.shape
-
-
-# In[27]:
-
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
-from sklearn import cross_validation
-
-
-# In[31]:
-
-X = tweetData
-Y = tweetClasses
-clf = OneVsRestClassifier(LinearSVC(random_state=0))
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.3, random_state=0)
-
-clf.fit(X_train, y_train)
-print clf.score(X_test, y_test)
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+if __name__ == "__main__":
+    main()
