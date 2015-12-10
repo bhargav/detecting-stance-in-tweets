e7c4bad75a3cc3a4980afc12f6684690e0499bf0
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2015-12-10 17:15:54 -0600

    Final Project - Some refactoring and do-over

---
+++ src/Model.py
@@ -0,0 +1,10 @@
+
+class Model():
+
+    def __init__(self, tweetID, target, tweet, stance):
+        self.tweetID = tweetID
+        self.target = target
+        self.tweet_content = tweet
+        self.stance = stance
+
+        self.original_tweet_content = list(tweet)
